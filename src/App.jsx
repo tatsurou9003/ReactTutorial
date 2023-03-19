3ef26c47b1fe332db6984d3b4749e43e8210d908
Tatsurosan <t.murakami@4nonome.com>
2023-03-19 16:01:22 +0000

    モック作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  const [incompleteTodos, setIncompleteTodos] = useState([
+    "あああああ",
+    "いいいいい",
+  ]);
+  const [completeTodos, setCompleteTodos] = useState(["ううううう"]);
+  return (
+    <>
+      <div className="input-area">
+        <input placeholder="Input TODO" />
+        <button>Add</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">Incomplete TODO</p>
+        {incompleteTodos.map((todo) => {
+          return (
+            <div key={todo} className="list-row">
+              <ul>
+                <li>{todo}</li>
+                <button>Complete</button>
+                <button>Delete</button>
+              </ul>
+            </div>
+          );
+        })}
+      </div>
+      <div className="complete-area">
+        <p className="title">Complete TODO</p>
+        {completeTodos.map((todo) => {
+          return (
+            <div key={todo} className="list-row">
+              <ul>
+                <li>{todo}</li>
+                <button>Restore</button>
+              </ul>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 };
